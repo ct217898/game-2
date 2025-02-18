546647a72bb90e17fabbe7433b041ff092af3087
Calvin Tate <ct217898@ebrschools.org>
2025-02-18 08:42:29 -0600

    road,car, edit camera

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-
+@export var offset = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	global_position.z = $"../Player".global_position.z + 3
+	global_position.z = $"../Player".global_position.z + offset
 	
 	
 	
